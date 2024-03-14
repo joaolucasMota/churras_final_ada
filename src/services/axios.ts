92d63eb293f60f5525241b04e4bcaab84392967e
Candido <antoniolmcandido@gmail.com>
2024-03-13 21:31:02 -0300

    :sparkles: config json-server no render.com

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,23 +1,25 @@
 import axios, { AxiosResponse } from 'axios';
 
-const BASE_URL = 'http://localhost:3000/';
+const api = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL,
+});
 
 export const postApi = (url: string, data: any) => {
-    return axios.post(`${BASE_URL}${url}`, data);
+    return api.post(url, data)
 }
 
-export const getApi = async <T>(url: string): Promise<AxiosResponse<T>> => {
-    return axios.get(`${BASE_URL}${url}`);
+export const getApi = async <T>(url: string): Promise<AxiosResponse<T, any>> => {
+    return api.get(url)
 }
 
 export const putApi = (url: string, data: any) => {
-    return axios.put(`${BASE_URL}${url}`, data);
+    return axios.put(url, data)
 }
 
 export const deleteApi = (url: string) => {
-    return axios.delete(`${BASE_URL}${url}`);
+    return axios.delete(url)
 }
 
 export const patchApi = (url: string, data: any) => {
-    return axios.patch(`${BASE_URL}${url}`, data);
+    return axios.patch(url, data)
 }
